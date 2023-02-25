5a4956b3ec97eb7eae2e19ec3cb9c9c763b019a6
ciucabianca <ciuca.bianca@yahoo.com>
2023-02-25 20:11:12 +0200

    add more dependencies and update structure

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,68 +1,64 @@
-import React, { Component } from "react";
-import { Input, Button } from "@material-ui/core";
+import React, { useState } from "react";
+import { Button } from "@material-ui/core";
 import "./Home.css";
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      url: "",
-    };
-  }
+function Home() {
+  let [url, setUrl] = useState("");
 
-  handleChange = (e) => this.setState({ url: e.target.value });
-
-  join = () => {
-    if (this.state.url !== "") {
-      var url = this.state.url.split("/");
+  const join = () => {
+    console.log("url", url);
+    if (url !== "") {
+      url.split("/");
       window.location.href = `/${url[url.length - 1]}`;
     } else {
-      var url = Math.random().toString(36).substring(2, 7);
-      window.location.href = `/${url}`;
+      const promiseUrl = new Promise((resolve, reject) => {
+        setUrl(Math.random().toString(36).substring(2, 7));
+        resolve(Math.random().toString(36).substring(2, 7));
+      });
+      promiseUrl.then((value) => {
+        window.location.href = `/${value}`;
+      });
     }
   };
 
-  render() {
-    return (
-      <div className="container2">
-        <div>
-          <h1 style={{ fontSize: "45px", color: "white" }}>@attention</h1>
-          <p style={{ fontWeight: "200", color: "white" }}>
-            Video conference website that makes your meetings more productive
-          </p>
-        </div>
-
-        <div
-          style={{
-            background: "white",
-            width: "30%",
-            height: "auto",
-            padding: "20px",
-            minWidth: "400px",
-            textAlign: "center",
-            margin: "auto",
-            marginTop: "100px",
-          }}>
-          <p
-            style={{
-              margin: 0,
-              fontWeight: "bold",
-              textAlign: "center",
-              fontSize: "30px",
-            }}>
-            Start a meeting
-          </p>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={this.join}
-            style={{ margin: "20px", textAlign: "center" }}>
-            Go
-          </Button>
-        </div>
+  return (
+    <div className="container2">
+      <div>
+        <h1 style={{ fontSize: "45px", color: "white" }}>@attention</h1>
+        <p style={{ fontWeight: "200", color: "white" }}>
+          Video conference website that makes your meetings more productive
+        </p>
       </div>
-    );
-  }
-}
 
+      <div
+        style={{
+          background: "white",
+          width: "30%",
+          height: "auto",
+          padding: "20px",
+          minWidth: "400px",
+          textAlign: "center",
+          margin: "auto",
+          marginTop: "100px",
+        }}>
+        <p
+          style={{
+            margin: 0,
+            fontWeight: "bold",
+            textAlign: "center",
+            fontSize: "30px",
+          }}>
+          Start a meeting
+        </p>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={join}
+          style={{ margin: "20px", textAlign: "center" }}>
+          Go
+        </Button>
+      </div>
+    </div>
+  );
+}
 export default Home;
